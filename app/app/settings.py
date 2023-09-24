71f8ae65ceb9d0ee68e4977b4ee2423c7ade87d9
Izabela Szczecinska <izabelaszczecinska5@gmail.com>
2023-09-24 23:40:28 +0200

    Docker requirements setup

--- app/app/settings.py
+++ app/app/settings.py
@@ -75,12 +75,15 @@ WSGI_APPLICATION = "app.wsgi.application"
 
 
 # Database
-# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "HOST": os.environ.get("DB_HOST"),
+        "NAME": os.environ.get("DB_NAME"),
+        "USER": os.environ.get("DB_USER"),
+        "PASSWORD": os.environ.get("DB_PASS"),
     }
 }
 
