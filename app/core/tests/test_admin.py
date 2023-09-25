ba766bc3a86b53b891af29994ab60150cd2886c7
Izabela Szczecinska <izabelaszczecinska5@gmail.com>
2023-09-25 17:05:19 +0200

    Admin tests added

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -9,12 +9,37 @@ from django.test import Client
 
 class AdminSiteTests(TestCase):
     """Tests for Django admin."""
+
     def setUp(self):
         """Create user and client."""
         self.client = Client()
+        self.admin_user = get_user_model().objects.create_superuser(
+            email="admin@example.com",
+            password="testpass123",
+        )
+        self.client.force_login(self.admin_user)
+        self.user = get_user_model().objects.create_user(
+            email="user@example.com", tier=None, password="testpass123", name="Test User"
+        )
+
     def test_users_lists(self):
+        """Test that users are listed on page."""
+        url = reverse("admin:core_user_changelist")
+        response = self.client.get(url)
+
+        self.assertContains(response, self.user.name)
         self.assertContains(response, self.user.email)
+
     def test_edit_user_page(self):
+        """Test the edit user page works."""
+        url = reverse("admin:core_user_change", args=[self.user.id])
+        response = self.client.get(url)
+
         self.assertEqual(response.status_code, 200)
+
     def test_create_user_page(self):
+        """Test the create user page works."""
+        url = reverse("admin:core_user_add")
+        response = self.client.get(url)
+
         self.assertEqual(response.status_code, 200)
