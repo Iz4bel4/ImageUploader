834802ce24a1a4d92b02753129d5e53fef8711d8
Izabela Szczecinska <izabelaszczecinska5@gmail.com>
2023-09-25 12:38:30 +0200

    Basic user model fields added

--- app/core/models.py
+++ app/core/models.py
@@ -1,8 +1,25 @@
+"""
+Database models.
+"""
+import uuid
+import os
+
+from django.conf import settings
 from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+)
 class UserManager(BaseUserManager):
     """Manager for users."""
 
     def create_user(self, email, tier=None, password=None, **extra_fields):
+        """Create, save and return a new user."""
+        if not email:
+            raise ValueError("User must have an email address.")
+        user = self.model(email=self.normalize_email(email), **extra_fields)
+        user.tier = tier
+        user.set_password(password)
         user.save(using=self._db)
 
         return user
@@ -11,3 +28,7 @@ class UserManager(BaseUserManager):
         return user
 class User(AbstractBaseUser, PermissionsMixin):
     """User in the system."""
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
---
+++ app/user/urls.py
@@ -0,0 +1,9 @@
+"""
+URL mappings for the user API.
+"""
+from django.urls import path
+
+from user import views
+
+
+app_name = "user"
