91552b047fe2b61c812451dbf5b9d9e5657983c0
Izabela Szczecinska <izabelaszczecinska5@gmail.com>
2023-09-25 09:03:50 +0200

    Defined user models

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+class UserManager(BaseUserManager):
+    """Manager for users."""
 
-# Create your models here.
+    def create_user(self, email, tier=None, password=None, **extra_fields):
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, password):
+        return user
+class User(AbstractBaseUser, PermissionsMixin):
+    """User in the system."""
