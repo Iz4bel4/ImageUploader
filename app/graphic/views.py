ca662e9e3ecb0918610c9f1bd6b31ced795bc670
Izabela Szczecinska <izabelaszczecinska5@gmail.com>
2023-09-27 15:34:11 +0200

    ModelViewSet retrieve/list override + thumbnail view logic added

--- app/graphic/views.py
+++ app/graphic/views.py
@@ -52,8 +52,46 @@ class GraphicViewSet(viewsets.ModelViewSet):
             options = {'size': (height, int(float(height) * ratio)), 'crop': True}
             thumb_url = get_thumbnailer(graphic.image).get_thumbnail(options).url
             data['thumbnail_' + str(height)] = 'http://127.0.0.1:8000' + thumb_url
+
     def try_adjust_original_link_presence(self, tier, data):
         """Add/remove/update original image link"""
+        if 'image' not in data:
+            return
+        
+        image_path = data['image']
+        if not tier.returns_original_image_link:
+            del data['image']
+        elif image_path is not None:
+            if 'http://127.0.0.1:8000' not in image_path:
+                data['image'] = 'http://127.0.0.1:8000' + image_path
+
+    def try_fill_with_links(self, tier, graphic, data):
+        if tier is not None:
+            # Thumbnail links
+            self.try_fill_with_thumbnail_links(tier, graphic, data)
+            # Original link
+            self.try_adjust_original_link_presence(tier, data)           
+
+    def list(self, request):
+        serializer = self.serializer_class(self.get_queryset(), many=True)
+        if serializer.data is not None:
+            for item in serializer.data:
+                graphic = Graphic.objects.get(id=item["id"])
+                tier = graphic.user.tier
+                self.try_fill_with_links(tier, graphic, item)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    def retrieve(self, request, pk=None):
+        try:
+            graphic = Graphic.objects.get(pk=pk)
+        except Graphic.DoesNotExist:
+            return Response({"detail": "Not found"}, status=status.HTTP_404_NOT_FOUND)
+
+        serializer = self.serializer_class(graphic)
+        modifiedData = serializer.data
+        tier = graphic.user.tier
+        self.try_fill_with_links(tier, graphic, modifiedData)
+        return Response(modifiedData, status=status.HTTP_200_OK)
     def _params_to_ints(self, qs):
         """Convert a list of strings to integers."""
         return [int(str_id) for str_id in qs.split(",")]
